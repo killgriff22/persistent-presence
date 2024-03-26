f41d19b11b8ed1198e4177620ce34b37274cb9c2
killgriff22 <kilgrif22033@gmail.com>
2024-03-26 14:47:06 -0400

    add patcher to main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from functions import *
 try:
     user.run(TOKEN)
-except IndexError:
-    with open(f"lib/{version}/site-packages/discord/utils.py", "r+") as f:
-        lines = f.readlines()
-        print(lines[1473])
+except:
+    f = open(f"lib/{version}/site-packages/discord/utils.py", "r")
+    lines = f.readlines()
+    f.close()
+    os.remove(f"lib/{version}/site-packages/discord/utils.py")
+    print(f"old lines: line 1474: {lines[1473] == old1474}, line 1478: {lines[1477] == old1478}")
+    if lines[1473] == new1474 and lines[1478] == new1478:
+        print("Already patched")
+        exit()
+    lines[1473] = "        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)').findall(login_page)[-2] + '.js'\n"
+    lines[1477] = "        return (build_file[build_index : build_index + 6])\n"
+    print(f"new lines: line 1474: {lines[1473] == new1474}, line 1478: {lines[1477] == new1478}")
+    if lines[1473] == new1474 and lines[1477] == new1478:
+        print("Patched: Check 1")
+    f = open(f"lib/{version}/site-packages/discord/utils.py", "w")
+    f.write("".join(lines))
+    f.close()
+    f = open(f"lib/{version}/site-packages/discord/utils.py", "r")
+    lines = f.readlines()
+    f.close()
+    print(f"new lines: line 1474: {lines[1473] == new1474}, line 1478: {lines[1477] == new1478}")
+    if lines[1473] == new1474 and lines[1477] == new1478:
+        print("Patched: Check 2")
+    else:
+        print("Failed to patch, uninstalling")
+        os.system(f"pip uninstall discord.py-self -y")
--- modules.py
+++ modules.py
@@ -7,6 +7,10 @@ import random
 import asyncio
 version = sys.version_info
 version = (f'python{version.major}.{version.minor}')
+old1474 = """        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)\.js').findall(login_page)[-2] + '.js'\n"""
+old1478 = """        return int(build_file[build_index : build_index + 6])\n"""
+new1474 = """        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)').findall(login_page)[-2] + '.js'\n"""
+new1478 = """        return (build_file[build_index : build_index + 6])\n"""
 try:
     import discord
     from discord.ext import tasks
