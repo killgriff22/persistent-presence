295b5fb3c38feb4f29c934dd31ea2448370ff39e
killgriff22 <kilgrif22033@gmail.com>
2024-03-26 14:16:41 -0400

    test

--- modules.py
+++ modules.py
@@ -1,10 +1,15 @@
 from config import *
 import os,time,subprocess,sys,random,asyncio
+print(sys.executable)
+exit()
 try:
     import discord
     from discord.ext import tasks
 except ImportError:
-    os.system('pip install discord')
+    os.system('pip install discord.py-self')
+    #determine python version by looking at "bin"
+    version = sys.executable.split('/')[-1]
+
     import discord
     from discord.ext import tasks
 
